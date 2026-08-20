83ce8ad05059db9c4cccb5f3c76c0cf691365815
ankitramola11 <ankitramola11@gmail.com>
2026-08-20 14:22:15 +0530

    Remove hardcoded API URLs from frontend

--- update-api.js
+++ update-api.js
@@ -5,7 +5,7 @@ const API_BASE = 'https://quiz-app-2l4p.onrender.com';
 const files = glob.sync('frontend/**/*.{html,js}');
 files.forEach(file => {
   let content = fs.readFileSync(file, 'utf8');
-  let newContent = content.replace(/https:\/\/ieee-quiz-c3u1\.onrender\.com/g, API_BASE);
+  let newContent = content.replace(/https:\/\/quiz-app-2l4p\.onrender\.com/g, '');
   
   if (content !== newContent) {
     fs.writeFileSync(file, newContent);
